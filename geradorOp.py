4d4d18d47ce9eca386c4b13679829457a3ce778b
unknown <alveslimneto@gmail.com>
2026-06-01 12:58:13 -0300

    trabalhando no gerador de op

--- geradorOp.py
+++ geradorOp.py
@@ -1,12 +1,39 @@
 import csv
 #import collections import deque
-def LastRow():
-    with open("ops.csv", "r", encoding="utf-8") as file:
+
+with open("generatorOp.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    rows = list(reader)
+    #print(rows)
+    
+if rows:
+    LastRow = rows[-1]
+    LastRow = int(LastRow[0]) + 1
+print(LastRow)
+dados = LastRow
+def save_csv(dados):
+    """Receives a list with data and save in data.csv"""
+    with open("generatorOp.csv", "a", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(dados)
+        print("Dados salvos com sucesso!")
+save_csv(dados)
+            
+def search_NextOp():
+    """Read last number of OP the archive ops.csv and return next id"""
+    """L> idea for read own main csv"""
+    try:
+        with open("ops.csv", "r", encoding="utf-8") as file: 
             reader = csv.reader(file)
             rows = list(reader)
             
-            if rows:
-                lastRow = rows[-1] 
-                lastRow_ = int(lastRow[0]) + 1
-                #print(lastRow_
-            
+            if rows and rows[-1]:
+                last_row = rows[-1]
+                return int(last_row[0]) + 1
+    except (FileNotFoundError, ValueError, IndexError):
+        pass 
+    return 1 # Return 1 if archivement not exist, if empty or fails
+
+    #proxima_op = banco_dados.search_NextOp() 
+    #op = ctk.CTkEntry(window, placeholder_text=f"Nº da OP (Sugestão: {proxima_op})...", width=250)
+    #op.pack(pady=5)
